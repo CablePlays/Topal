81ce43d495e252470c26d88747f6bc6fad37379a
Cable <cableplays1912@gmail.com>
2023-09-02 08:29:43 +0200

    Added Sequel Links System

--- public/javascripts/global/awards.js
+++ public/javascripts/global/awards.js
@@ -1,30 +1,11 @@
+const _BASIC_LINK_ID = "basic-link"
+const _INSTRUCTOR_LINK_ID = "instructor-link"
+const _LEADER_LINK_ID = "leader-link"
+
 function setAwardName(name) {
     byId("award-title").innerHTML = name + " Award"
     byId("award-info-title").innerHTML = name + " Award Info"
 }
-
-window.addEventListener("load", () => {
-    const awardLinksElement = byId("award-links")
-    const titleStart = byId("title-start")
-    let next = titleStart
-
-    while (true) {
-        next = next.nextElementSibling
-        const currentElement = next
-
-        if (currentElement == null) break
-        if (currentElement.tagName !== "H2") continue;
-
-        const div = createElement("div", {
-            p: awardLinksElement, onClick() {
-                currentElement.scrollIntoView({ behavior: "smooth" })
-            }
-        })
-
-        createElement("p", { p: div, t: next.innerHTML })
-        createElement("div", { c: "material-icons", p: div, t: "keyboard_arrow_down" })
-    }
-})
 
 function _setRating(rating, val) {
     const ratingElement = byId(rating)
@@ -77,3 +58,62 @@ function includePoints() {
         pointsDisplay
     ])
 }
+
+function showSequels(instructor, leader) {
+    const pathSplit = location.pathname.split("/")
+    let awardType = pathSplit[pathSplit.length - 1]
+    let sequelType
+
+    for (let st of ["instructor", "leader"]) {
+        if (awardType.endsWith("-" + st)) {
+            awardType = awardType.substring(0, awardType.length - st.length - 1)
+            sequelType = st
+            break
+        }
+    }
+
+    let pathRoot = ""
+
+    for (let i = 1; i < pathSplit.length - 1; i++) { // start 1 -> skip blank
+        pathRoot += "/" + pathSplit[i]
+    }
+
+    if (sequelType) {
+        byId(_BASIC_LINK_ID).addEventListener("click", () => window.location.href = `${pathRoot}/${awardType}`)
+        byId(_BASIC_LINK_ID).style.display = "flex"
+    }
+    if (instructor && sequelType !== "instructor") {
+        byId(_INSTRUCTOR_LINK_ID).addEventListener("click", () => window.location.href = `${pathRoot}/${awardType}-instructor`)
+        byId(_INSTRUCTOR_LINK_ID).style.display = "flex"
+    }
+    if (leader && sequelType !== "leader") {
+        byId(_LEADER_LINK_ID).addEventListener("click", () => window.location.href = `${pathRoot}/${awardType}-leader`)
+        byId(_LEADER_LINK_ID).style.display = "flex"
+    }
+}
+
+window.addEventListener("load", () => {
+
+    /* Log Headings */
+
+    const awardLinksElement = byId("award-links")
+    const titleStart = byId("title-start")
+    let next = titleStart
+
+    while (true) {
+        next = next.nextElementSibling
+        const currentElement = next
+
+        if (currentElement == null) break
+        if (currentElement.tagName !== "H2") continue;
+
+        const div = createElement("div", {
+            p: awardLinksElement, onClick() {
+                currentElement.scrollIntoView({ behavior: "smooth" })
+            }
+        })
+
+        createElement("p", { p: div, t: next.innerHTML })
+        createElement("div", { c: "material-icons", p: div, t: "keyboard_arrow_down" })
+    }
+})
